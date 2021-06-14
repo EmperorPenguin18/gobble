f6acc15acab60eaee1faf771702fbf06d8f36487
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-06-13 23:19:34 -0400

    Improved error handling

--- gobble.rs
+++ gobble.rs
@@ -2,20 +2,23 @@
 //License is available at
 //https://github.com/EmperorPenguin18/gobble/blob/main/LICENSE
 
-extern crate xcb;
-use std::{env, process, thread, time};
+use std::{env, process, thread, time, fmt};
 
-fn main() {
-    let (conn, _screen_num) = xcb::Connection::connect(None).unwrap();
-    let win = xcb::get_input_focus(&conn).get_reply().unwrap().focus();
-    let args: Vec<String> = env::args().collect();
-    let mut command = process::Command::new(&args[1]);
-    command.args(&args[2 .. args.len()]);
+fn main() -> Result<(), anyhow::Error> {
+    let (conn, _screen_num) = xcb::Connection::connect(None)?;
+    let win = xcb::get_input_focus(&conn).get_reply()?.focus();
+    let args: Vec = env::args().collect();
 
-    xcb::unmap_window(&conn, win);
+    xcb::unmap_window_checked(&conn, win).request_check()?;
     conn.flush();
-    let stat: i32 = command.status().unwrap().code().unwrap();
-    xcb::map_window(&conn, win);
+
+    let stat: i32 = process::Command::new(&args[1])
+        .args(&args[2..])
+        .status()?
+        .code()
+        .ok_or(fmt::Error)?;
+
+    xcb::map_window_checked(&conn, win).request_check()?;
     conn.flush();
 
     thread::sleep(time::Duration::from_millis(10));
