78130a5ac3e17e6c322ef1314fa0cf4d819f1616
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-08-11 16:32:59 -0400

    Update gobble.rs

--- gobble.rs
+++ gobble.rs
@@ -5,22 +5,32 @@
 use std::{env, fmt, process};
 
 fn main() -> Result<(), anyhow::Error> {
-    let (conn, _screen_num) = xcb::Connection::connect(None)?;
-    let win = xcb::get_input_focus(&conn).get_reply()?.focus();
     let args: Vec = env::args().collect();
-
-    xcb::unmap_window_checked(&conn, win).request_check()?;
-    conn.flush();
-
-    let exit_code = command(args);
-
-    xcb::map_window_checked(&conn, win).request_check()?;
-    conn.flush();
-
-    match exit_code {
-        Ok(s) => process::exit(s),
-        Err(e) => return Err(e),
-    };
+    let wayland = env::var_os("WAYLAND_DISPLAY");
+    
+    if wayland == None { // X11
+        let (conn, _screen_num) = xcb::Connection::connect(None)?;
+        let win = xcb::get_input_focus(&conn).get_reply()?.focus();
+        
+        xcb::unmap_window_checked(&conn, win).request_check()?;
+        conn.flush();
+        
+        let exit_code = command(args);
+        
+        xcb::map_window_checked(&conn, win).request_check()?;
+        conn.flush();
+        
+        match exit_code {
+            Ok(s) => process::exit(s),
+            Err(e) => return Err(e),
+        };
+    } else { // Wayland
+        let exit_code = command(args);
+        match exit_code {
+            Ok(s) => process::exit(s),
+            Err(e) => return Err(e),
+        };
+    }
 }
 
 fn command(args: Vec) -> Result {
