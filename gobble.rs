00adb01f9a9c19c6b4d7b8dce961f3911b319882
morganamilo <morganamilo@archlinux.org>
2021-10-07 22:29:44 +0100

    cargo fmt

--- gobble.rs
+++ gobble.rs
@@ -7,24 +7,26 @@ use std::{env, fmt, process};
 fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
     let wayland = env::var_os("WAYLAND_DISPLAY");
-    
-    if wayland == None { // X11
+
+    if wayland == None {
+        // X11
         let (conn, _screen_num) = xcb::Connection::connect(None)?;
         let win = xcb::get_input_focus(&conn).get_reply()?.focus();
-        
+
         xcb::unmap_window_checked(&conn, win).request_check()?;
         conn.flush();
-        
+
         let exit_code = command(args);
-        
+
         xcb::map_window_checked(&conn, win).request_check()?;
         conn.flush();
-        
+
         match exit_code {
             Ok(s) => process::exit(s),
             Err(e) => return Err(e),
         };
-    } else { // Wayland
+    } else {
+        // Wayland
         let exit_code = command(args);
         match exit_code {
             Ok(s) => process::exit(s),
