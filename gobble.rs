3eb8e2ff50033e83a80e6ea4af9744d147eec3bf
VuiMuich <vuimuich@quantentunnel.de>
2023-08-23 17:07:37 +0200

    bunch of refactoring to make clippy happy

--- gobble.rs
+++ gobble.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), anyhow::Error> {
     let mut flag_version = false;
     let mut flag_help = false;
     while args.len() > 1 {
-        if args[1].chars().nth(0).unwrap() == '-' {
+        if args[1].starts_with('-') {
             if args[1].chars().nth(1).unwrap() == 'o' {
                 flag_overlap = true;
             } else if args[1].chars().nth(1).unwrap() == 'v' {
@@ -29,99 +29,105 @@ fn main() -> Result<(), anyhow::Error> {
         args.remove(1);
     }
 
-    let exit_code = if flag_help == true {
+    let exit_code = if flag_help {
         println!("gobble - hide your current window while using an external program");
-        println!("");
+        println!();
         println!("USAGE:");
         println!("  gobble [OPTIONS] CMD ...");
-        println!("");
+        println!();
         println!("OPTIONS:");
-        println!("");
+        println!();
         println!("  -h      Displays the help message you're seeing now");
         println!("  -v      Displays the software version");
         println!("  -o      Uses overlap mode");
-        println!("");
+        println!();
         println!("See the manual for more information");
         0
-    } else if flag_version == true {
+    } else if flag_version {
         println!("gobble v1.2");
         println!("See https://github.com/EmperorPenguin18/gobble/releases for more info");
         0
-    } else if wayland == None {
-        // X11
-        let (conn, screen_num) = Connection::connect(None)?;
-        let win = conn
-            .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
-            .focus();
-        if flag_overlap == false {
-            // ensure child was spawned before we hide the window
-            let mut child: process::Child;
-            if args.len() > 1 {
-                child = command(&args)?;
-            } else {
-                process::exit(0);
-            }
-
-            conn.send_request_checked(&x::UnmapWindow { window: win });
-            let _ = conn.flush();
-
-            let exit_code = child.wait()?.code().unwrap_or(1);
-
-            conn.send_request_checked(&x::MapWindow { window: win });
-            let _ = conn.flush();
-            exit_code
-        } else {
-            let mut child: process::Child;
-            if args.len() > 1 {
-                child = command(&args)?;
-            } else {
-                process::exit(0);
-            }
-
-            let translate = conn.wait_for_reply(
-                conn.send_request(&x::TranslateCoordinates {
-                    src_window: win,
-                    dst_window: conn
-                        .get_setup()
-                        .roots()
-                        .nth(screen_num as usize)
-                        .unwrap()
-                        .root(),
-                    src_x: 0,
-                    src_y: 0,
-                }),
-            )?; //Translates relative position to absolute position
-            let geometry = conn.wait_for_reply(conn.send_request(&x::GetGeometry {
-                drawable: x::Drawable::Window(win),
-            }))?;
-            let values = [
-                x::ConfigWindow::X((translate.dst_x() as i32).into()).into(),
-                x::ConfigWindow::Y((translate.dst_y() as i32).into()).into(),
-                x::ConfigWindow::Width((geometry.width() as u16).into()).into(),
-                x::ConfigWindow::Height((geometry.height() as u16).into()).into(),
-            ];
-
-            let mut newwin = conn
-                .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
-                .focus();
-            while win == newwin {
-                newwin = conn
-                    .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
-                    .focus();
-            }
-            conn.send_request_checked(&x::ConfigureWindow {
-                window: newwin,
-                value_list: &values,
-            });
-
-            child.wait()?.code().unwrap_or(1)
-        }
+    } else if wayland.is_none() {
+        gobble_on_x11(flag_overlap, &args)?
     } else {
-        // Wayland
-        command(&args)?.wait()?.code().unwrap_or(1)
+        gobble_on_wayland(&args)?
     };
 
     process::exit(exit_code);
+}
+
+fn gobble_on_wayland(args: &[String]) -> Result<i32, anyhow::Error> {
+    Ok(command(args)?.wait()?.code().unwrap_or(1))
+}
+
+fn gobble_on_x11(flag_overlap: bool, args: &[String]) -> Result<i32, anyhow::Error> {
+    let (conn, screen_num) = Connection::connect(None)?;
+    let win = conn
+        .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
+        .focus();
+    Ok(if flag_overlap {
+        let mut child: process::Child;
+        if args.len() > 1 {
+            child = command(args)?;
+        } else {
+            process::exit(0);
+        }
+
+        let translate = conn.wait_for_reply(
+            conn.send_request(&x::TranslateCoordinates {
+                src_window: win,
+                dst_window: conn
+                    .get_setup()
+                    .roots()
+                    .nth(screen_num as usize)
+                    .unwrap()
+                    .root(),
+                src_x: 0,
+                src_y: 0,
+            }),
+        )?; //Translates relative position to absolute position
+        let geometry = conn.wait_for_reply(conn.send_request(&x::GetGeometry {
+            drawable: x::Drawable::Window(win),
+        }))?;
+        let values = [
+            x::ConfigWindow::X(i32::from(translate.dst_x())),
+            x::ConfigWindow::Y(i32::from(translate.dst_y())),
+            x::ConfigWindow::Width(geometry.width().into()),
+            x::ConfigWindow::Height(geometry.height().into()),
+        ];
+
+        let mut newwin = conn
+            .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
+            .focus();
+        while win == newwin {
+            newwin = conn
+                .wait_for_reply(conn.send_request(&x::GetInputFocus {}))?
+                .focus();
+        }
+        conn.send_request_checked(&x::ConfigureWindow {
+            window: newwin,
+            value_list: &values,
+        });
+
+        child.wait()?.code().unwrap_or(1)
+    } else {
+        // ensure child was spawned before we hide the window
+        let mut child: process::Child;
+        if args.len() > 1 {
+            child = command(args)?;
+        } else {
+            process::exit(0);
+        }
+
+        conn.send_request_checked(&x::UnmapWindow { window: win });
+        let _ = conn.flush();
+
+        let exit_code = child.wait()?.code().unwrap_or(1);
+
+        conn.send_request_checked(&x::MapWindow { window: win });
+        let _ = conn.flush();
+        exit_code
+    })
 }
 
 fn command(args: &[String]) -> Result<process::Child, anyhow::Error> {
